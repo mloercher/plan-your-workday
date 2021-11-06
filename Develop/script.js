9b9a7e62a004f126fc3cc50f24347b054be983a6
Mark <markloercher@gmail.com>
2021-11-06 11:04:40 -0400

    highlighting text areas

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,73 @@
+// Get date at top of screen
 const today = moment().format('LL');
 $( "#currentDay").append("Today is "+ today);
-var currentHour = $(".hour").text();
 
 // getting current, past and future hours to highlight appropriately
-var ninthHour = $("hour-9-number").text();
+// var ninthHour = $("hour-9-number").text();
 
-window.onload = function updateTimeSlotItems(){
-    console.log(ninthHour);
-    console.log(moment().hour());
-    if(ninthHour = moment().hour() + ":00"){
-        console.log("yes");
-        document.getElementById("hour-9-text-area").style.backgroundColor = "#ff6961";
+var thisHour = [$("#hour-9-number").text().split(":")[0], $("#hour-10-number").text().split(":")[0],$("#hour-11-number").text().split(":")[0]]
+
+window.onload = function updateTimeSlotItems (){
+    console.log(thisHour);
+    const currentHour = moment().hour();
+    console.log(currentHour)
+    $("#hour-9-text-area").each( function(){
+    if (parseInt(thisHour[0]) < currentHour){
+        console.log(true);
+        $(this).addClass("past")
+    };
+    if (parseInt(thisHour[0]) == currentHour){
+        console.log("present");
+        $(this).addClass("present")
     }
     else{
-        document.getElementById("hour-9-text-area").style.backgroundColor = "#ffffff";
-        console.log("no");
-    };
-};
+        console.log(false);
+        $(this).addClass("future")
+    }
+    });
+    $("#hour-10-text-area").each( function(){
+        if (parseInt(thisHour[1]) < currentHour){
+            console.log(true);
+            $(this).addClass("past")
+        };
+        if (parseInt(thisHour[1]) == currentHour){
+            console.log("present");
+            $(this).addClass("present")
+        }
+        else{
+            console.log(false);
+            $(this).addClass("future")
+        }
+        });
+         $("#hour-11-text-area").each( function(){
+            if (parseInt(thisHour[2]) < currentHour){
+                console.log(true);
+                $(this).addClass("past")
+            };
+            if (parseInt(thisHour[2]) === currentHour){
+                console.log("present");
+                $(this).addClass("present")
+            }
+            else{
+                console.log(false);
+                $(this).addClass("future")
+            }
+            });
+    
+}
+
+// window.onload = function updateTimeSlotItems(){
+//     console.log(ninthHour);
+//     console.log(moment().hour());
+//     if(ninthHour == moment().hour() + ":00"){
+//         console.log("yes");
+//         document.getElementById("hour-9-text-area").style.backgroundColor = "#ff6961";
+//     }
+//     else{
+//         document.getElementById("hour-9-text-area").style.backgroundColor = "#ffffff";
+//         console.log("no");
+//     };
+// };
 
 // var ninthHour = $("hour-9").text();
 // var timeSlot;
@@ -27,4 +78,3 @@ window.onload = function updateTimeSlotItems(){
     // else {
     //     currentHour.closest().addClass("past")
     //     };
-
